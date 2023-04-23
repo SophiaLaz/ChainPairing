099849c0032021845d1144a9214492b5f3909949
SophiaLaz <113467577+SophiaLaz@users.noreply.github.com>
2023-04-23 17:27:28 +0300

    Update launch.sh

--- language model/launch.sh
+++ language model/launch.sh
@@ -1,1 +1,1 @@
-python3 embedder.py --input sequences/f8.fasta --model prot_bert --output embeddings/residue_embeddings.h5 --per_protein 1
+python3 embedder.py --input sequences/example.fasta --model prot_bert --output embeddings/residue_embeddings.h5 --per_protein 1
