db20185d9acb4b237571b70eddfe6d9375437bc7
SophiaLaz <113467577+SophiaLaz@users.noreply.github.com>
2023-04-23 18:11:45 +0300

    Update conjugacy_table.py

--- statistical ananlysis/conjugacy_table.py
+++ statistical ananlysis/conjugacy_table.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 df = pd.read_csv("all_pared.csv")
+
 table = df.value_counts(['v_call_heavy', 'v_call_light'])
 table = table.to_frame().reset_index().rename(columns={0: 'count'})
 table = table.sort_values(by=['v_call_heavy', 'v_call_light'])
 table = table.pivot_table(index='v_call_light', columns='v_call_heavy', values='count', fill_value=0)
 table.to_csv('conjugacy_table.csv')
+
+table = df.value_counts(['v_call_heavy', 'v_call_light_short'])
+table = table.to_frame().reset_index().rename(columns={0: 'count'})
+table = table.sort_values(by=['v_call_heavy', 'v_call_light_short'])
+table = table.pivot_table(index='v_call_light_short', columns='v_call_heavy', values='count', fill_value=0)
+table.to_csv('conjugacy_table_short.csv')
