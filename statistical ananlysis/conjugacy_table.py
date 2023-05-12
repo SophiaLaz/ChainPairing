630e9db97488c9b1b52b6f4023d97d6109bbb3a2
SophiaLaz <lazareva.sone4ka@yandex.ru>
2023-05-12 14:29:44 +0300

    Статистистический анализ для v- и j-генов.

--- statistical ananlysis/conjugacy_table.py
+++ statistical ananlysis/conjugacy_table.py
@@ -1,15 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("all_pared.csv")
+file_names = ['v_call.csv', 'j_call.csv']
+columns = [['v_call_heavy', 'v_call_light'], ['j_call_heavy', 'j_call_light']]
 
-table = df.value_counts(['v_call_heavy', 'v_call_light'])
-table = table.to_frame().reset_index().rename(columns={0: 'count'})
-table = table.sort_values(by=['v_call_heavy', 'v_call_light'])
-table = table.pivot_table(index='v_call_light', columns='v_call_heavy', values='count', fill_value=0)
-table.to_csv('conjugacy_table.csv')
+for file_name, column in zip(file_names, columns):
+    df = pd.read_csv(file_name)
 
-table = df.value_counts(['v_call_heavy', 'v_call_light_short'])
-table = table.to_frame().reset_index().rename(columns={0: 'count'})
-table = table.sort_values(by=['v_call_heavy', 'v_call_light_short'])
-table = table.pivot_table(index='v_call_light_short', columns='v_call_heavy', values='count', fill_value=0)
-table.to_csv('conjugacy_table_short.csv')
+    table = df.value_counts(column)
+    table = table.to_frame().reset_index().rename(columns={0: 'count'})
+    table = table.sort_values(by=column)
+    table = table.pivot_table(index=column[1], columns=column[0], values='count', fill_value=0)
+    table.to_csv('conjugacy_table/' + file_name)
--- statistical ananlysis/data_v_cal.py
+++ statistical ananlysis/data_v_cal.py
@@ -4,18 +4,26 @@ import os
 parth = 'data_set/paired/'
 filenames = os.listdir(parth)
 df = pd.DataFrame()
+columns = ['v_call_heavy', 'j_call_heavy',
+           'v_call_light', 'j_call_light']
 for file_i in filenames:
     try:
-        df_i = pd.read_csv(parth+file_i, skiprows=1, usecols=['v_call_heavy', 'v_call_light'])
+        df_i = pd.read_csv(parth+file_i, skiprows=1, usecols=columns)
         df_i = df_i.dropna()
+
         mask1 = df_i['v_call_heavy'].apply(lambda x: x.split(sep='-')[0][:-1] == 'IGHV')
         mask2 = df_i['v_call_light'].apply(lambda x: x.split(sep='-')[0][:3] in ['IGK', 'IGL'])
-        df_i = df_i[mask1 & mask2]
+        mask3 = df_i['j_call_heavy'].apply(lambda x: x.split(sep='*')[0][:-1] == 'IGHJ')
+        mask4 = df_i['j_call_light'].apply(lambda x: x.split(sep='*')[0][:-1] in ['IGKJ', 'IGLJ'])
+        df_i = df_i[mask1 & mask2 & mask3 & mask4]
         df_i['v_call_heavy'] = df_i['v_call_heavy'].str.split('-').str[0]
         df_i['v_call_light'] = df_i['v_call_light'].str.split('-').str[0]
-        df_i['v_call_light_short'] = df_i['v_call_light'].str[:3]
+        df_i['j_call_heavy'] = df_i['v_call_heavy'].str.split('*').str[0]
+        df_i['j_call_light'] = df_i['v_call_light'].str.split('*').str[0]
         df = pd.concat([df, df_i], ignore_index=True)
+
     except Exception as except_i:
         print(f'File {file_i} is not correct: {except_i}')
 
-df.to_csv('all_pared.csv', index=False)
+df.to_csv('v_call.csv', columns=['v_call_heavy', 'v_call_light'], index=False)
+df.to_csv('j_call.csv', columns=['j_call_heavy', 'j_call_light'], index=False)
